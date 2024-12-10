649e083b31f6e7bb43b8e23f90d71074f22598ed
root <10114@holbertonstudents.com>
2024-12-10 18:37:39 +0000

    Add functions to add left and right nodes thus enhancing proper usage of binary trees

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+*
+*
+*
+*
+*/
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if(node == NULL || node->parent != NULL)
+		return (0);
+	else
+		return (1);
+
+}
